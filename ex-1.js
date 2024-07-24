b8f87b42811d3ae99f694e98645367afffea57ef
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-07-24 14:15:36 +0700

    finished assignemnt

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,48 @@
+/*
+**Exercise 1:**
+
+เรามี Array `customers` ซึ่งบรรจุรายชื่อสมาชิก ให้ใช้ Bubble Sort Algorithm เพื่อเรียงลำดับสมาชิกตามลำดับตัวอักษร
+
+เช่น ถ้า `customers = ["Somchai", "Arthit", "Suchada", "Napasorn", "Pimchanok"]`
+เมื่อ Algorithm นี้ทำงานจะแสดงผลลัพธ์ออกมาบนหน้าจอเป็น
+`[ "Arthit", "Napasorn", "Pimchanok", "Somchai", "Suchada"]`
+
+```jsx
 function sortCustomerName(customers) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
 }
 
 // ตอบคำถามตรงนี้จ้า
+```
+
+ยังจำได้ไหมว่า Bubble Sort Algorithm มี Big O เป็นเท่าไร เพราะอะไร
+ให้เขียนอธิบายใน comment แนบมาข้างใต้โค้ดตรงส่วนที่ระบุว่า **“ตอบคำถามตรงนี้จ้า”**
+*/
+
+// [     [ shuffle  -1-i<-|PUSH  ]  -1 ]
+// use tmpVar as triangle shuffle
+
+function sortCustomerName(customers) {
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0; i < customers.length - 1; i++) {
+    for (let j = 0; j < customers.length - 1 - i; j++) {
+      let tmpVar = "";
+      if (customers[j] > customers[j + 1]) {
+        tmpVar = customers[j];
+        customers[j] = customers[j + 1];
+        customers[j + 1] = tmpVar;
+      }
+    }
+  }
+  return customers;
+}
+
+const customers = ["Somchai", "Arthit", "Suchada", "Napasorn", "Pimchanok"];
+console.log(sortCustomerName(customers));
+// `[ "Arthit", "Napasorn", "Pimchanok", "Somchai", "Suchada"]`
+
+// ตอบคำถามตรงนี้จ้า
+/*
+Big O = n^2
+เพราะเป็น nested loop
+*/
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,79 @@
+/*
+**Exercise 2:** 
+
+1. เรามี Array `students` ซึ่งบรรจุข้อมูลนักเรียน ให้เขียน Algorithm เพื่อเรียงลำดับนักเรียนตามคะแนนสอบ 
+โดยเรียงจากคนที่สอบได้คะแนนสูงสุดไปยังคนที่สอบได้คะแนนต่ำสุด เช่น
+    
+    ```jsx
+    [
+        { id: "S001", name: "Anan", score: 85 },
+        { id: "S002", name: "Boon", score: 92 },
+        { id: "S003", name: "Chai", score: 78 },
+        { id: "S004", name: "Dao", score: 95 },
+        { id: "S005", name: "Ek", score: 88 },
+        { id: "S006", name: "Fah", score: 80 }
+    ];
+    ```
+    
+    เมื่อ Algorithm นี้ทำงานจะเห็นผลลัพธ์ดังนี้
+    
+    ```jsx
+    [
+        { id: "S004", name: "Dao", score: 95 },
+        { id: "S002", name: "Boon", score: 92 },
+        { id: "S005", name: "Ek", score: 88 },
+        { id: "S001", name: "Anan", score: 85 },
+        { id: "S006", name: "Fah", score: 80 },
+        { id: "S003", name: "Chai", score: 78 }
+    ]
+    ```
+    
+    ```jsx
+    function sortStudentsByScore(students) {
+      // เริ่มเขียนโค้ดตรงนี้จ้า
+    };
+    ```
+    
+    - Hint
+        
+        เราสามารถใช้ Bubble Sort Algorithm ในการแก้ปัญหาข้อนี้ได้ 
+        เพิ่มแค่การเข้าถึงข้อมูลที่อยู่ในรูปแบบ Key-Value Pair เท่านั้น
+*/
+
+// [     [ shuffle  -1-i<-|PUSH  ]  -1 ]
+// use tmpVar as triangle shuffle
+
 function sortStudentsByScore(students) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0; i < students.length - 1; i++) {
+    for (let j = 0; j < students.length - 1 - i; j++) {
+      let tmpVar = null;
+      if (students[j].score < students[j + 1].score) {
+        tmpVar = students[j];
+        students[j] = students[j + 1];
+        students[j + 1] = tmpVar;
+      }
+    }
+  }
+  return students;
 }
+
+const students = [
+  { id: "S001", name: "Anan", score: 85 },
+  { id: "S002", name: "Boon", score: 92 },
+  { id: "S003", name: "Chai", score: 78 },
+  { id: "S004", name: "Dao", score: 95 },
+  { id: "S005", name: "Ek", score: 88 },
+  { id: "S006", name: "Fah", score: 80 },
+];
+
+console.log(sortStudentsByScore(students));
+
+// [
+//     { id: "S004", name: "Dao", score: 95 },
+//     { id: "S002", name: "Boon", score: 92 },
+//     { id: "S005", name: "Ek", score: 88 },
+//     { id: "S001", name: "Anan", score: 85 },
+//     { id: "S006", name: "Fah", score: 80 },
+//     { id: "S003", name: "Chai", score: 78 }
+// ]
